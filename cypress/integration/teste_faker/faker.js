ee068db0b3426b57e0fd3cdb132c3fa504e3447b
Gustavo Coelho <90300226+CoelhoGustavo1@users.noreply.github.com>
2022-05-02 21:36:11 -0300

    Modulo com faker funcionando

---
+++ cypress/integration/teste_faker/faker.js
@@ -0,0 +1,29 @@
+describe("Primeiro teste utilizando o FAKER", () => {
+  const { faker } = require("@faker-js/faker");
+  faker.locale = "pt_BR";
+
+  const randomName = faker.name.findName(); // Rowan Nikolaus
+  const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+  const randomPhoneNumber = faker.phone.phoneNumber(); // (279) 329-8663 x30233
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const phonenumber = faker.phone.phoneNumber();
+  const city = faker.address.city();
+  const state = faker.address.state();
+  const country = faker.address.country();
+
+  it("Teste com o FAKER", () => {
+    cy.log(randomName);
+    cy.log(randomEmail);
+    cy.log(randomPhoneNumber);
+    cy.log(firstName);
+    cy.log(lastName);
+    cy.log(phonenumber);
+    cy.log(city);
+    cy.log(state);
+    cy.log(country);
+  });
+});
+
+//Util Links
+//https://github.com/faker-js/faker
